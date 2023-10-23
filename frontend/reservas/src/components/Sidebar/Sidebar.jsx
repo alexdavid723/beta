652750fc3a68629ff56bad1d7d27ed5aaf878484
alexdavid723 <alexdavidxela@gmail.com>
2023-10-22 22:15:39 -0500

    modificando y agregando columnas

--- frontend/reservas/src/components/Sidebar/Sidebar.jsx
+++ frontend/reservas/src/components/Sidebar/Sidebar.jsx
@@ -8,6 +8,14 @@ const Home = () => {
             <li><a href="#">Acerca de</a></li>
             <li><a href="#">Servicios</a></li>
             <li><a href="#">Contacto</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Contacto</a></li>
           </ul>
         </div>
         <div className="content">
